55a7499e4ad10182d9a84ce3c2494231db6fd3b5
rgarciap <rjgpinel@gmail.com>
2021-11-03 16:19:25 +0100

    agent/utils: fix bug

--- bc/agent/utils.py
+++ bc/agent/utils.py
@@ -1,5 +1,5 @@
 def add_to_stack(elem, stack, shift=1):
     if stack is not None:
-        stack[:-shift] = stack[shift:]
+        stack[:-shift] = stack[shift:].clone()
         stack[-shift:] = elem.clone()
     return stack
